51812b5f394cfb0a83adaf9fed569acdc6768486
Nathan Humphrey <nathpaulhump@gmail.com>
2025-09-06 10:20:52 -0600

    Add week two lesson starters

---
+++ lesson-04-starter/src/main.js
@@ -0,0 +1,27 @@
+console.log('Lesson 04 starter loaded');
+
+// Instructor TODO:
+// 1. Simple if
+// 2. if-else
+// 3. Nested if-else
+// 4. while loop
+// 5. do-while loop
+// 6. for loop
+
+// Student TODO:
+// 7. Snippet with bugs for debugging practice
+// Snippet with bugs for debugging practice - uncomment when ready
+/*
+const num = 10;
+
+if (num < 5) { // Intentional bug: should be num > 5
+  console.log('num is greater than 5');
+} else {
+  console.log('num is 5 or less');
+}
+
+for (let k = 0; k < 3; k = k - 1) { // Intentional bug: k should increment
+  console.log(k);
+}
+*/
+// 8. Debugging practice
