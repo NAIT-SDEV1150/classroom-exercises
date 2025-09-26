306cf56129b318e62072dfca3226f08d35cf5bc8
Nathan Humphrey <nathpaulhump@gmail.com>
2025-09-26 15:48:31 -0600

    Add lesson 09 example

---
+++ lesson-09-complete/src/main.js
@@ -0,0 +1,61 @@
+console.log('Lesson 10 starter loaded');
+
+// ============== Propagation demo
+
+// 1. Select required elements
+const log = document.getElementById('log');
+const outer = document.getElementById('outer');
+const inner = document.getElementById('inner');
+const button = document.getElementById('btn-propagate');
+
+// 2. Add event listeners
+
+// 2.1 Outer div - using a named function
+function outerClick() {
+  log.textContent += 'Outer clicked (capture) | ';
+}
+
+outer.addEventListener('click', outerClick); // TODO: Demo capture option
+
+// 2.2 Inner div - using an anonymous function
+inner.addEventListener('click', function () {
+  log.textContent += 'Inner clicked (bubble) | ';
+});
+
+// 2.3 Button - using an arrow function
+button.addEventListener('click', (event) => {
+  log.textContent += 'Button clicked | ';
+  // TODO: Demo stopping propagation
+  event.stopPropagation();
+});
+
+// ============== Gallery demo
+
+// 1. Select required elements
+const thumbnails = document.querySelector('.thumbnails');
+const mainImage = document.getElementById('main-image');
+const viewer = document.querySelector('.viewer');
+const closeBtn = document.getElementById('close-viewer');
+
+// 2. Add event listeners
+
+// 2.1 Thumbnails container - using an arrow function
+thumbnails.addEventListener('click', (event) => {
+  if (event.target.tagName === 'IMG') {
+    mainImage.src = event.target.src;
+    viewer.classList.add('show');
+  }
+});
+
+// 2.2 Close button - using an arrow function
+closeBtn.addEventListener('click', () => {
+  viewer.classList.remove('show');
+});
+
+// Student TODO: Add event listener to document, which closes
+// the viewer when the Escape key is pressed
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    viewer.classList.remove('show');
+  }
+});
---
+++ lesson-09-starter/src/main.js
@@ -0,0 +1,26 @@
+console.log('Lesson 10 starter loaded');
+
+// ============== Propagation demo
+
+// 1. Select required elements
+
+// 2. Add event listeners
+
+// 2.1 Outer div - using a named function
+
+// 2.2 Inner div - using an anonymous function
+
+// 2.3 Button - using an arrow function
+
+// ============== Gallery demo
+
+// 1. Select required elements
+
+// 2. Add event listeners
+
+// 2.1 Thumbnails container - using an arrow function
+
+// 2.2 Close button - using an arrow function
+
+// Student TODO: Add event listener to document, which closes
+// the viewer when the Escape key is pressed
