c15f7c4cc439efbe9a4c711d50067a213c101370
Nathan Humphrey <nathpaulhump@gmail.com>
2025-10-23 18:39:27 -0600

    Add Lesson 19 example

---
+++ lesson-19-starter/src/main.js
@@ -0,0 +1,5 @@
+// Import the user-card component to register the custom element
+
+// Create an additional user card using HTML and append it to the main element
+
+// Create another user card using JavaScript DOM API only and append it to the main element
